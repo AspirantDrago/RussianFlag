501d2b8ab377d3207f884b683946b47d7e973bb9
AspirantDrago <sanekclubstr@mail.ru>
2019-08-19 17:20:56 +0500

    Draw flagpole

--- program.py
+++ program.py
@@ -8,6 +8,10 @@ screen = pygame.display.set_mode(size)
 def draw():
     screen.fill([255, 255, 255])
     pygame.display.set_caption("Российский флаг")
+    # Рисуем флагшток
+    flagpole_color = [226, 132, 50]
+    flagpole_rect = [(40, 20), (5, height - 20 * 2)]
+    pygame.draw.rect(screen, flagpole_color, flagpole_rect)
 
 draw()
 
