ccf6873674e3d2495696530b5ba913486ae6de8a
AspirantDrago <sanekclubstr@mail.ru>
2019-08-19 17:22:39 +0500

    Draw flagpole

--- program.py
+++ program.py
@@ -12,6 +12,10 @@ def draw():
     flagpole_color = [226, 132, 50]
     flagpole_rect = [(40, 20), (5, height - 20 * 2)]
     pygame.draw.rect(screen, flagpole_color, flagpole_rect)
+    # Рисуем красный сегмент (цвет по ГОСТу Р 51130-98)
+    segment_red_color = [213, 43, 30]
+    segment_red_rect = [(30, 30 + 2 * 2 * 40), (9 * 40, 2 * 40)]
+    pygame.draw.rect(screen, segment_red_color, segment_red_rect)
 
 draw()
 
