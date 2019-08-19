e0373a2326ccadf4b331f9e335704b5244300b78
AspirantDrago <sanekclubstr@mail.ru>
2019-08-19 17:34:32 +0500

    Draw border flag

--- program.py
+++ program.py
@@ -12,6 +12,11 @@ def draw():
     flagpole_color = [226, 132, 50]
     flagpole_rect = [(40, 20), (5, height - 20 * 2)]
     pygame.draw.rect(screen, flagpole_color, flagpole_rect)
+    # Рисуем границы флага
+    segment_border_color = [0] * 3
+    segment_border_rect = [(30, 30), (9 * 40, 3 * 2 * 40)]
+    pygame.draw.rect(screen, segment_border_color, segment_border_rect, 1)
+
 
 draw()
 
